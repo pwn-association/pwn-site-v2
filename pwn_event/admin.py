2a00c65db30ace328dc696d98ade7f5340af8b61
ssorin <ssorin@dwdlc.com>
2023-11-24 09:00:05 +0100

    Customisation de l'admin de l'app pwn_event
    - customisation de l'admin des events (list, detail, recherche ...)
    - customisation de l'admin speaker (liste + recherche)

--- pwn_event/admin.py
+++ pwn_event/admin.py
@@ -7,11 +7,43 @@ from .models.tag import Tag
 
 
 class EventAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = "date"
+    list_filter = ['is_publish',]
+    search_fields = ['title', 'speakers__first_name', 'speakers__last_name',]
+    list_display = ['title', 'date', 'get_speakers', 'place', 'is_publish']
+    fieldsets =  [
+        (
+            None,
+            {
+                "fields": [
+                    ("title", "is_publish"),
+                    "date",
+                    "image", "excerpt", "description",
+                    "place", "speakers", "tags",
+                    "season",
+                ],
+            },
+        ),
+        (
+            "Publication",
+            {
+                "classes": ["collapse"],
+                "fields": ["start_publication", "end_publication"],
+            },
+        ),
+    ]
+    filter_horizontal = ('tags', 'speakers')
+
+    def get_speakers(self, obj):
+        intervenant = ''
+        for speaker in obj.speakers.all():
+            intervenant += f"{speaker.first_name} {speaker.last_name}"
+        return intervenant
 
 
 class SpeakerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['first_name', 'last_name', ]
+    search_fields = ['first_name', 'last_name', ]
 
 
 class PlaceAdmin(admin.ModelAdmin):
