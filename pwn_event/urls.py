f96af77f95491861a28bdb1eea23729554843e61
ssorin <ssorin@dwdlc.com>
2024-06-26 11:03:07 +0200

    Customisation de l'admin de l'app pwn_event
    - Meilleur gestion des affichage des événements passées par saison. Les événéments passées s'affichent par ordre chronologique, on affiche seulement les événements passés

--- pwn_event/urls.py
+++ pwn_event/urls.py
@@ -13,28 +13,34 @@ from .views.event import EventListView, EventDetailView, EventBySeasonListView
 app_name = 'pwn_event'
 
 urlpatterns = [
-    # # Places
-    # path('places/', PlaceListView.as_view(), name='place_list'),
-    # path('places/<slug:slug>/', PlaceDetailView.as_view(), name='place_detail'),
-    #
-    # # Speakers
-    # path('speakers/', SpeakerListView.as_view(), name='speaker_list'),
-    # path('speakers/<slug:slug>/', SpeakerDetailView.as_view(), name='speaker_detail'),
-    #
-    # # Season
-    # path(r'seasons/', SeasonListView.as_view(), name='season_list'),
-    # path(r'seasons/<slug:slug>/', SeasonDetailView.as_view(), name='season_detail'),
-    #
-    # # TAg
-    # path(r'tags/', TagListView.as_view(), name='tag_list'),
+    path('', EventBySeasonListView.as_view(), name='event_season_list'),
+    path('<slug:slug>/', EventDetailView.as_view(), name='event_detail'),
+]
+
+
+# Liste des urls existants mais non utilisées dans ce projet
+urlpatterns_not_used = [
+    # Places
+    path('places/', PlaceListView.as_view(), name='place_list'),
+    path('places/<slug:slug>/', PlaceDetailView.as_view(), name='place_detail'),
+
+    # Speakers
+    path('speakers/', SpeakerListView.as_view(), name='speaker_list'),
+    path('speakers/<slug:slug>/', SpeakerDetailView.as_view(), name='speaker_detail'),
+
+    # Season
+    path(r'seasons/', SeasonListView.as_view(), name='season_list'),
+    path(r'seasons/<slug:slug>/', SeasonDetailView.as_view(), name='season_detail'),
+
+    # TAg
+    path(r'tags/', TagListView.as_view(), name='tag_list'),
     path(r'tags/<slug:slug>/', TagDetailView.as_view(), name='tag_detail'),
 
     # Events
-    # path('', EventListView.as_view(), name='event_list'),
-    path('', EventBySeasonListView.as_view(), name='event_season_list'),
+    path('', EventListView.as_view(), name='event_list'),
+    path('event-season', EventBySeasonListView.as_view(), name='event_season_list'),
     path('<slug:slug>/', EventDetailView.as_view(), name='event_detail'),
 
-    # # Home
-    # path('', TemplateView.as_view(template_name='pwn_event/home.html'), name='event_home'),
+    # Home
+    path('', TemplateView.as_view(template_name='pwn_event/home.html'), name='event_home'),
 ]
-
