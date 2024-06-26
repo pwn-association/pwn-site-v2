b98e4b715f9c396d963584c95af479052717e439
ssorin <ssorin@dwdlc.com>
2024-06-26 23:20:32 +0200

    #10 - event preview
    les supers users ont acces aux confs non publiés en preview

--- pwn_event/templatetags/pwn_event.py
+++ pwn_event/templatetags/pwn_event.py
@@ -23,15 +23,19 @@ def get_tags(context, template='pwn_event/template_tags/tags.html'):
             'context_tag': context.get('tag')}
 
 
-@register.inclusion_tag('pwn_event/template_tags/dummy.html')
-def get_last_events(number=5, template='pwn_event/template_tags/last_events.html'):
+@register.inclusion_tag('pwn_event/template_tags/dummy.html', takes_context=True)
+def get_last_events(context, number=3, template='pwn_event/template_tags/last_events.html'):
     """
     Return the most recent creations.
     """
 
     season = Season.objects.get(start_date__lte=now(), end_date__gte=now())
     last_event_time = now().replace(hour=00, minute=00, second=00)
-    events = Event.published.filter(season=season, date__gte=last_event_time)[:number]
+    request = context['request']
+    if request.user.is_superuser:
+        events = Event.objects.filter(season=season, date__gte=last_event_time)[:number]
+    else:
+        events = Event.published.filter(season=season, date__gte=last_event_time)[:number]
 
     return {'template': template, 'events': events}
 
