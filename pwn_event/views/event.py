4a45d124cc7d058dec6ddc8bae0bf4973d055b65
ssorin <ssorin@dwdlc.com>
2025-06-12 17:51:02 +0200

    Fix future events query to include events from upcoming years
    Corrected the filter logic in the `futur_events` query to account for events in future years by updating `date__year` to `date__year__gte`.

--- pwn_event/views/event.py
+++ pwn_event/views/event.py
@@ -43,7 +43,7 @@ class EventBySeasonListView(ListView):
                 start_date__year=now().year, start_date__month=9
             )
             context["futur_events"] = Event.objects.filter(
-                date__year=now().year, date__month__gte=9
+                date__year__gte=now().year, date__month__gte=9
             )
         except Season.DoesNotExist:
             context["futur_season"] = None
